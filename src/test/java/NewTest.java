54100651d6ee91b800ad5cd0379b1be69dffdf2a
knikishin <kirill.nikishin.1987@gmail.com>
2024-02-08 17:22:52 +0300

    get new test

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,11 +1,17 @@
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class NewTest {
 
     @Test
     void successfulSearchTest() {
-        Assertions.assertTrue(3 > 2);
-    }
-
+            open("https://www.google.com/");
+            $("[name=q]").setValue("selenide").pressEnter();
+            Configuration.timeout = 50000;  // без timeout не успевает прогрузиться страница
+            $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        }
 }
